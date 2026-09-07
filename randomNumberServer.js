726f66ed824e42ba1f51505b8cf4decd05a2a6cf
Nimret <nimretbedi@gmail.com>
2026-09-07 17:43:32 +0530

    switcg case added in randomNumberServer

--- randomNumberServer.js
+++ randomNumberServer.js
@@ -1,35 +1,54 @@
-const console = require("console");
 const http = require("http");
-const fs = require("node:fs");
+const fs = require("fs");
+const url = require("url");
+
 const server = http.createServer((req, res) => {
-  res.write("<h1>Random Number Server</h1>\n");
   const date = new Date().toLocaleString();
   const ip = req.socket.remoteAddress;
+  const myURL = url.parse(req.url, true);
+
+  switch (myURL.pathname) {
+    case "/":
+      const userName = myURL.query.username;
+      res.end(`<h1>Hello, ${userName}</h1>`);
+      break;
+
+    case "/profile":
+      res.end("You are in profile page");
+      break;
+
+    case "/home":
+      res.end("You are in home page");
+      break;
+
+    case "/search":
+      fs.appendFile(
+        "log.txt",
+        `Timestamp=${date} URL=${req.url} Parameters=${JSON.stringify(myURL.query)}\n`,
+        (err) => {
+          if (err) console.error("Error writing to log file:", err);
+        },
+      );
+      res.end(`<p>Search query: ${myURL.query.username}</p>`);
+      break;
+
+    default:
+      res.end("404 page not found");
+  }
 
   fs.appendFile(
     "server.txt",
-    `Request received at ${date} from ${ip} \n`,
+    `Request received at ${date} from ${ip}\n`,
     (err) => {
-      err && console.log("errors", err);
+      if (err) console.error("Error writing to server log:", err);
     },
   );
-  const interval = setInterval(() => {
-    let randomNum = Math.ceil(Math.random() * 100);
-    res.write(`<p> Random Number: ${randomNum}</p>\n`);
-  }, 200);
-  setTimeout(() => {
-    clearInterval(interval);
-    res.end("<h3>Done sending random numbers!</h3>");
-  }, 20000);
 });
 
 server.listen(8000, () => {
-  fs.appendFile(
-    "server.txt",
-    `Server started at ${new Date().toLocaleString()}\n`,
-    (err) => {
-      err && console.log("errors", err);
-    },
-  );
+  const startTime = new Date().toLocaleString();
+  fs.appendFile("server.txt", `Server started at ${startTime}\n`, (err) => {
+    if (err) console.error("Error writing server start log:", err);
+  });
   console.log("Server started");
 });
